2b1850b87360793398234c7df426311d394215b5
Alex Zenin <alex.zenin@convertdigital.com.au>
2024-12-03 22:18:09 +1100

    day 2 complete

--- Day2/day2_2.mjs
+++ Day2/day2_2.mjs
@@ -1,6 +1,8 @@
 import fs from 'fs'
 
 const lines = fs.readFileSync('./input_data.txt', 'utf8')
+const input = lines.split('\n')
+input.pop()
 
 /**
  *
@@ -8,77 +10,26 @@ const lines = fs.readFileSync('./input_data.txt', 'utf8')
  * @returns {boolean}
  */
 function isSafe(line, hasProblemDamperBeenUsed = false) {
+  let direction = 0
 
-  let increasing = true
-  let decreasing = true
-
-//   console.log('\n')
-//     console.log(line)
   for (let i = 1; i < line.length; i++) {
     const diff = line[i] - line[i-1]
-    // console.log(line[i-1], line[i])
-
-    if (diff === 0 || Math.abs(diff) > 3) {
+    if (diff === 0 || Math.abs(diff) > 3 || diff * direction < 0) {
         if (!hasProblemDamperBeenUsed) {
-            return isSafe(line.toSpliced(i - 1, 1), true) || isSafe(line.toSpliced(i, 1), true)
+            return isSafe(line.toSpliced(i - 1, 1), true)
+                || isSafe(line.toSpliced(i, 1), true)
+                // This is in case of a change in direction with the first number. eg 7 4 6 7 8 10 13 15
+                || (i-2 === 0 && isSafe(line.toSpliced(i-2, 1), true))
         }
         return false
     }
-
-    if (diff > 0 ) {
-        if (increasing === false && !hasProblemDamperBeenUsed) {
-            return isSafe(line.toSpliced(i-1, 1), true) || isSafe(line.toSpliced(i, 1), true)
-        }
-        decreasing = false
-    }
-
-    if (diff < 0) {
-        if (decreasing === false && !hasProblemDamperBeenUsed) {
-            return isSafe(line.toSpliced(i-1, 1), true) || isSafe(line.toSpliced(i, 1), true)
-        }
-        increasing = false
-    }
+    direction = diff
   }
-
-
-  if (increasing || decreasing) {
-    // console.log('safe')
-  } else {
-    // console.log('\n')
-    // console.log(line)
-    // console.log('unsafe')
-  }
-  return increasing || decreasing
+  return true
 }
 
-const input = lines.split('\n')
-input.pop()
-
-console.log(input.length)
-
 const output = input.reduce((total, line) => {
-//   const lineArray = line.split(' ')
-//   let isSafeFlag = false
-//   if (isSafe(lineArray)) {
-//     isSafeFlag = true
-//   }
-//   for (let i = 0; i < lineArray.length; i++) {
-//     if (isSafeFlag) {
-//       break
-//     }
-//     if (isSafe(lineArray.toSpliced(i, 1))) {
-//       isSafeFlag = true
-//     }
-//   }
-//   if (isSafeFlag) {
-//     return total + 1
-//   }
-//   return total
-  if (isSafe(line.split(' '))) {
-    return total + 1
-  }
-//   console.log('\n', line, 'unsafe')
-  return total
+  return isSafe(line.split(' ')) ? total + 1 : total
 }, 0)
 
 console.log(output)
